a814127ccfdff171f9963cc1d6f2a55e71b9d6b8
HENRIQUE KEMPNER DA MOTTA <henrique_k_motta@estudante.sesisenai.org.br>
2025-10-02 16:48:15 -0300

    conteudo sobre print e variaveis

---
+++ aula1_variaveis.py
@@ -0,0 +1,10 @@
+# Podemos definir variáveis com o nome dela, seguido pelo
+# sinal de igual, que significa "atribuir" e depois pelo valor.
+# nome = "Henrique" <- Crie uma variavel nome que recebe o valor "Ronaldo"
+
+nome = ("Henrique")
+salario = 2500.00
+dias_trabalhados = 10
+print("O funcionário é",nome)
+print("Ele trabalhou",dias_trabalhados,"dias")
+print("O salário é",salario)
