38e8b8dd7440b0449359e770cd99d296a4bf53c6
HENRIQUE KEMPNER DA MOTTA <henrique_k_motta@estudante.sesisenai.org.br>
2025-10-03 16:48:35 -0300

    Aula 2: Variaveis, input e calculos

---
+++ aula 2.py
@@ -0,0 +1,24 @@
+# Para criar uma variavel, colocamos:
+# o nome dela
+# o simbolo de atribuição (-)
+# o valo inicial dela
+
+# Uma variavel chamada nome que recebe o meu nome
+nome = "Henrique"
+print("Olá, me chamo",nome)
+print("O tipo de variavel é",type(nome))
+print()
+# Uma variavel do tipo inteiro
+quantidade_alunos = 16
+print("Estamos em",quantidade_alunos,"alunos na sala")
+print("O tipo de variavel é",type(quantidade_alunos))
+print()
+# Uma variavel do tipo...
+salario_bruto = 2500
+print("O salário mínimo é R$",salario_bruto)
+print("O tipo de variavel é",type(salario_bruto))
+print()
+# Uma variavel do tipo bool
+is_aberta = False
+print("A porta está aberta?",is_aberta)
+print("O tipo de variavel é",type(is_aberta))
