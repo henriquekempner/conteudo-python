4f10b771b808557dc87c5b7daafc1152526ae3e5
HENRIQUE KEMPNER DA MOTTA <henrique_k_motta@estudante.sesisenai.org.br>
2025-10-09 16:53:10 -0300

    exercícios 4 e 11

---
+++ exercicios/exercicio_8.py
@@ -0,0 +1,4 @@
+salario = float(input("Quanto que voce recebe?"))
+horas = float(input("Quantas horas voce trabalha por dia?"))
+horas_trabalhadas = salario/horas
+print("O seu salário por hora é de: ",horas_trabalhadas)
---
+++ exercicios/sequencial/exercicio_7.py
@@ -0,0 +1,3 @@
+lado = int(input("Digite o tamanho do quadrado "))
+resultado = lado * lado * 2
+print("Digite o valor da area: ",resultado)
